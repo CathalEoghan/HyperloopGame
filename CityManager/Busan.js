0b0c7b21bbc84bb071b9f04c358565cb3c2eb9bb
CathalEoghan <cathaleoghanmcglone@gmail.com>
2026-06-20 14:49:37 +0100

    Added Busan, and the Seafood Restaurant

---
+++ CityManager/Busan.js
@@ -0,0 +1,13 @@
+
+// Busan
+import { City } from "./City.js";
+import { seafoodRestaurant } from "../StoreManager/SeafoodRestaurant.js";
+
+export const Busan = new City(
+    "Busan",
+    4000000,
+    [seafoodRestaurant],
+    "South Korea",
+    2,
+    "Busan is home to the Centum City Shinsegae, which is officially certified by Guinness World Records as the world's largest department store.'"
+);
