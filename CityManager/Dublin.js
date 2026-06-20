2cae5bca02b732b81ca99e4ef65a15ade85a445a
CathalEoghan <cathaleoghanmcglone@gmail.com>
2026-06-20 17:28:47 +0100

    Added Dublin and Pub. Adjusted Karaoke Bar price

---
+++ CityManager/Dublin.js
@@ -0,0 +1,13 @@
+
+// Dublin
+import { City } from "./City.js";
+import { pub } from "../StoreManager/Pub.js";
+
+export const Dublin = new City(
+    "Dublin",
+    15000000,
+    [cupcakeStore],
+    "Ireland",
+    2100000,
+    "Dublin's famous Guinness brewery at St. James's Gate originally signed an unbelievable 9,000-year lease for the land back in 1759, and it still costs them just £45 per year to rent the site."
+    );
---
+++ StoreManager/Pub.js
@@ -0,0 +1,9 @@
+
+import { Store } from "../StoreManager/Store.js";
+
+export const pub = new Store(
+    "Pub",
+    27000,
+    "food",
+    750
+);
