bc0a01393ee947671567fa8e8efc12df388b935d
40235069 <cmcglone06@qub.ac.uk>
2026-06-26 16:31:09 +0100

    Tied up rank and xp to frontend

--- hyperloop-ui/src/App.jsx
+++ hyperloop-ui/src/App.jsx
@@ -10,20 +10,53 @@ import './App.css'
 function App() {
 
 const [balance, setBalance] = useState(0);
+const [totalCashEarned, setTotalCashEarned] = useState(0);
+const [rankSet, setRankSet] = useState(1);
+const [xp, setXp] = useState(0);
+const [xpAtRankUp, setXpAtRankUp] = useState(0);
+const [xpNeeded, setXpNeeded] = useState(5);
 const [activeTab, setActiveTab] = useState("Home");
+
+function calculateNextRankXP(rank) {
+    let xpNeeded = 5;
+    let difference = 3;
+    for (let i = 2; i < rank; i++) {
+        xpNeeded += difference;
+        difference++;
+    }
+    return xpNeeded;
+}
 
 useEffect(() => {
   setInterval(() => {
-    setBalance(prev => prev + (8000 / 86400))
-  }, 1000)
-}, [])
+    const incomePerSecond = 8000 / 86400;
+
+  setTotalCashEarned(prev => {
+      const newTotal = prev + incomePerSecond;
+      const newXP = Math.floor(newTotal);
+      setXp(newXP);
+      setRankSet(currentRank => {
+    const needed = calculateNextRankXP(currentRank + 1);
+    if (newXP >= needed) {
+        setXpAtRankUp(newXP);
+        setXpNeeded(calculateNextRankXP(currentRank + 2));
+        return currentRank + 1;
+    }
+    return currentRank;
+});
+      return newTotal;   
+    });          
+
+    setBalance(prev => prev + incomePerSecond);
+  }, 1000);             
+}, []);                 
 
 return (
 
 <div className= "App">
-<TopBanner terminalName="Hyperloop Central" balance={balance} />
+<TopBanner terminalName="Hyperloop Central" balance={balance} rank={rankSet} />
 
-<ExperienceBar current={3} max={10} />
+<ExperienceBar current={xp - xpAtRankUp} max={xpNeeded - xpAtRankUp} />
 {activeTab === "Cities" && <CitiesPage purchasedCities={[{name: "London"}]} />}
 {activeTab === "Development" && <DevelopmentPage purchasedDevelopments={[{name: "Cupcake Store"}]} />}
 <BottomNav activeTab={activeTab} onSelect={setActiveTab}>
