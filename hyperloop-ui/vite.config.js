f3ff7f9b46bd01391735e8186d4d304888349f6a
40235069 <cmcglone06@qub.ac.uk>
2026-06-27 14:50:32 +0100

    More bug fixes

--- hyperloop-ui/vite.config.js
+++ hyperloop-ui/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'Managers': new URL('../Managers', import.meta.url).pathname,
+      'CityManager': new URL('../CityManager', import.meta.url).pathname,
+      'DevelopmentManager': new URL('../DevelopmentManager', import.meta.url).pathname,
+      'UpgradeManager': new URL('../UpgradeManager', import.meta.url).pathname,
+    }
+  }
 })
