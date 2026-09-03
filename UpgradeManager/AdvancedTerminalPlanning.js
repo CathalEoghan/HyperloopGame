0f3a8ead1423e6ea5e6dd75d493470fce0c2a206
CathalEoghan <cathaleoghanmcglone@gmail.com>
2026-09-03 01:32:15 +0100

    Parsing error fixes

--- UpgradeManager/AdvancedTerminalPlanning.js
+++ UpgradeManager/AdvancedTerminalPlanning.js
@@ -5,8 +5,7 @@ import { Upgrade } from "./Upgrade.js";
 export const advancedTerminalPlanning = new Upgrade(
     "Advanced Terminal Planning",
     155000,
-    "Service",
     "Infrastructure",
     "freeRerollOnRankUp",
     1
-);
+)
--- UpgradeManager/AdvertisingCampaignSpain.js
+++ UpgradeManager/AdvertisingCampaignSpain.js
@@ -5,7 +5,8 @@ import { Upgrade } from '../UpgradeManager/Upgrade.js'
 export const advertisingCampaignSpain = new Upgrade(
     "Advertising Campaign: Spain",
     87000,
+    "Service",
     "countryAdvertisingBoost",
     0.25,
     "Spain"
-);
+)
